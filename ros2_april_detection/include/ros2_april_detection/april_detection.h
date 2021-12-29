b37641980ee0691f0e5b7376b4f684b3e23adddd
David Paz <dpazruiz@ucsd.edu>
2021-12-28 21:55:53 -0800

    adds dependencies

--- ros2_april_detection/include/ros2_april_detection/april_detection.h
+++ ros2_april_detection/include/ros2_april_detection/april_detection.h
@@ -1,6 +1,11 @@
 #ifndef APRIL_DETECTION_H
 #define APRIL_DETECTION_H
-
+#include <opencv2/opencv.hpp>
+#include <apriltag/apriltag.h>
+#include <apriltag/apriltag_pose.h>
+#include <apriltag/tag36h11.h>
+#include <apriltag/common/getopt.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 class AprilDetection{
