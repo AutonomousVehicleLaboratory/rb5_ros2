4a34b62027640479c5161023788d4dda27cd95e0
David Paz <dpazruiz@ucsd.edu>
2022-02-09 04:06:00 +0000

    begins integration between offline mapping and gtsam

--- ros2_april_slam/src/april_mapper.cpp
+++ ros2_april_slam/src/april_mapper.cpp
@@ -16,101 +16,72 @@
 // using std::placeholders::_1;
 using namespace std;
 
-class AprilMapper : public rclcpp::Node{
-  public:
-    AprilMapper() : Node("april_slam"){
+// mapping
+AprilSlam l_map;
+Vector3 odom = {0.0, 0.0, 0.0};
+Vector2 velocities = {0.0, 0.0};
+int optimizer_trigger = 0, imu_trigger = 0;
 
-      pose_sub_ = this->create_subscription<geometry_msgs::msg::PoseStamped>(
-        "/april_poses", 10, [this](geometry_msgs::msg::PoseStamped::SharedPtr msg) {aprilCallback(msg); } );
-      
-      imu_sub_ = this->create_subscription<sensor_msgs::msg::Imu>(
-        "/imu", 1, [this](sensor_msgs::msg::Imu::SharedPtr msg) {imuCallback(msg); } );
-      
-      imu_init = false;
-      return;
-    }
+bool imu_init;
+double t_prev;
 
+//callbacks
+void aprilCallback(const geometry_msgs::msg::PoseStamped::SharedPtr msg){
+  
+  ++optimizer_trigger;
+  double x = msg->pose.position.x;
+  double y = msg->pose.position.z;
+  // RCLCPP_INFO(this->get_logger(), "r: %f, bearing: %f", range, bearing);
 
+  auto t_start = std::chrono::high_resolution_clock::now();
+  l_map.updateMeasurement(odom, Vector2(x, y), stoi(msg->header.frame_id));
+  auto t_stop = std::chrono::high_resolution_clock::now();
+  odom[0] = odom[1] = odom[2] = 0.0;
+  auto update_dt = std::chrono::duration_cast<std::chrono::microseconds>(t_stop - t_start);
 
-  private:
-
-    // Bagfile reader
-    // gtsam optimization 
-    AprilSlam map;
-    Vector3 odom = {0.0, 0.0, 0.0};
-    Vector2 velocities = {0.0, 0.0};
-    int optimizer_trigger = 0, imu_trigger = 0;
-
-    bool imu_init;
-    double t_prev;
-
-    // subscribers
-    rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr pose_sub_;
-    rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_sub_;
-
-    // publishers
-
-    //callbacks
-    void aprilCallback(const geometry_msgs::msg::PoseStamped::SharedPtr msg){
-      
-      ++optimizer_trigger;
-      double x = msg->pose.position.x;
-      double y = msg->pose.position.z;
-      // double range = sqrt( pow(x, 2) + pow(y, 2) );
-      // double bearing = atan(y / x);
-      // RCLCPP_INFO(this->get_logger(), "r: %f, bearing: %f", range, bearing);
-
-      auto t_start = std::chrono::high_resolution_clock::now();
-      map.updateMeasurement(odom, Vector2(x, y), stoi(msg->header.frame_id));
-      auto t_stop = std::chrono::high_resolution_clock::now();
-      odom[0] = odom[1] = odom[2] = 0.0;
-      auto update_dt = std::chrono::duration_cast<std::chrono::microseconds>(t_stop - t_start);
-      // std::cout << "Update dt: " << update_dt.count() << std::endl;
-
-      if (optimizer_trigger % 20 == 0 ){
-        t_start = std::chrono::high_resolution_clock::now();
-        int graph_size = map.optimizeGraph();
-        t_stop = std::chrono::high_resolution_clock::now();
-        update_dt = std::chrono::duration_cast<std::chrono::microseconds>(t_stop - t_start);
-        std::cout << "Optimization dt: " << update_dt.count() << " | ";
-        std::cout << "Graph size: " << graph_size << std::endl;
-        optimizer_trigger = 0;
-      }
+  if (optimizer_trigger % 20 == 0 ){
+    t_start = std::chrono::high_resolution_clock::now();
+    int graph_size = l_map.optimizeGraph();
+    t_stop = std::chrono::high_resolution_clock::now();
+    update_dt = std::chrono::duration_cast<std::chrono::microseconds>(t_stop - t_start);
+    std::cout << "Optimization dt: " << update_dt.count() << " | ";
+    std::cout << "Graph size: " << graph_size << std::endl;
+    optimizer_trigger = 0;
+  }
 
 
-      return;
-    }
-    
-    void imuCallback(const sensor_msgs::msg::Imu::SharedPtr msg){
+  return;
+}
 
-      ++imu_trigger;
-      if (!imu_init) {
-        imu_init = true;
-        t_prev = msg->header.stamp.sec + msg->header.stamp.nanosec * 1e-9;
-        return;
-      }
-      
-      
-      double t = msg->header.stamp.sec + msg->header.stamp.nanosec * 1e-9;
-      double dt = t - t_prev;
-      t_prev = t;
+void imuCallback(const sensor_msgs::msg::Imu::SharedPtr msg){
 
-      double w_z = msg->angular_velocity.z;
-      double a_x = msg->linear_acceleration.x;
-      double a_y = msg->linear_acceleration.y;
+  ++imu_trigger;
+  if (!imu_init) {
+    imu_init = true;
+    t_prev = msg->header.stamp.sec + msg->header.stamp.nanosec * 1e-9;
+    return;
+  }
+  
+  
+  double t = msg->header.stamp.sec + msg->header.stamp.nanosec * 1e-9;
+  double dt = t - t_prev;
+  t_prev = t;
 
-      velocities[0] += a_x * dt;
-      velocities[1] += a_y * dt;
+  double w_z = msg->angular_velocity.z;
+  double a_x = msg->linear_acceleration.x;
+  double a_y = msg->linear_acceleration.y;
 
-      odom[0] +=  velocities[0] * dt; // x = x(0) + a_x * dt^2
-      odom[1] +=  velocities[1] * dt; // y = y(0) + a_y * dt^2
-      odom[2] +=  w_z * dt; // yaw
+  velocities[0] += a_x * dt;
+  velocities[1] += a_y * dt;
 
-      return;
-    }
+  odom[0] +=  velocities[0] * dt; // x = x(0) + a_x * dt^2
+  odom[1] +=  velocities[1] * dt; // y = y(0) + a_y * dt^2
+  odom[2] +=  w_z * dt; // yaw
 
-};
-std::vector<std::shared_ptr<rosbag2_storage::SerializedBagMessage>> get_messages(std::string bag_path)
+  return;
+}
+
+std::vector<std::shared_ptr<rosbag2_storage::SerializedBagMessage>> getMessages(std::string bag_path)
 {
   std::vector<std::shared_ptr<rosbag2_storage::SerializedBagMessage>> table_msgs;
   auto storage = std::make_shared<rosbag2_storage_plugins::SqliteStorage>();
@@ -124,7 +95,7 @@ std::vector<std::shared_ptr<rosbag2_storage::SerializedBagMessage>> get_messages
 }
 
 template<typename MessageT>
-std::vector<std::shared_ptr<MessageT>> process_topic(std::vector<std::shared_ptr<rosbag2_storage::SerializedBagMessage>> bag, std::string topic_name){
+std::vector<std::shared_ptr<MessageT>> processTopic(std::vector<std::shared_ptr<rosbag2_storage::SerializedBagMessage>> bag, std::string topic_name){
   rosbag2_test_common::MemoryManagement memory_management;
   auto topic_messages = std::vector<std::shared_ptr<MessageT>>();
 
@@ -138,8 +109,9 @@ std::vector<std::shared_ptr<MessageT>> process_topic(std::vector<std::shared_ptr
   return topic_messages;
 }
 
-void process_imu_camera(std::vector<std::shared_ptr<rosbag2_storage::SerializedBagMessage>> bag){
+void processImuCamera(std::vector<std::shared_ptr<rosbag2_storage::SerializedBagMessage>> bag){
 
+  // bagfile processing
   rosbag2_test_common::MemoryManagement memory_management;
   auto imu_messages = std::vector<std::shared_ptr<sensor_msgs::msg::Imu>>();
   auto april_messages = std::vector<std::shared_ptr<geometry_msgs::msg::PoseStamped>>();
@@ -167,10 +139,12 @@ void process_imu_camera(std::vector<std::shared_ptr<rosbag2_storage::SerializedB
     det_ts = april_messages[0]->header.stamp.sec + april_messages[0]->header.stamp.nanosec * 1e-9;
     if (imu_ts < det_ts){
       // process imu
+      imuCallback(imu_messages[0]);
       imu_messages.erase(imu_messages.begin());
     }
     else{
       // process detection
+      aprilCallback(april_messages[0]);
       april_messages.erase(april_messages.begin());
 
     }
@@ -179,7 +153,7 @@ void process_imu_camera(std::vector<std::shared_ptr<rosbag2_storage::SerializedB
   while (imu_messages.size()){
     imu_ts = imu_messages[0]->header.stamp.sec + imu_messages[0]->header.stamp.nanosec * 1e-9;
     det_ts = april_messages[0]->header.stamp.sec + april_messages[0]->header.stamp.nanosec * 1e-9;
-    std::cout << "I: " << imu_ts << std::endl;
+    imuCallback(imu_messages[0]);
     imu_messages.erase(imu_messages.begin());
 
   }
@@ -187,7 +161,7 @@ void process_imu_camera(std::vector<std::shared_ptr<rosbag2_storage::SerializedB
   while (april_messages.size()){
     imu_ts = imu_messages[0]->header.stamp.sec + imu_messages[0]->header.stamp.nanosec * 1e-9;
     det_ts = april_messages[0]->header.stamp.sec + april_messages[0]->header.stamp.nanosec * 1e-9;
-    std::cout << "A: " << det_ts << std::endl;
+    aprilCallback(april_messages[0]);
     april_messages.erase(april_messages.begin());
 
   }
@@ -200,9 +174,8 @@ int main(int argc, char* argv[]){
   // rclcpp::shutdown();
   std::string dir_path = "/home/dpaz/Documents/gtsam_data/rosbag2_2022_02_09-02_25_26";
 
-  auto bag = get_messages(dir_path);
-  // auto camera_topic = process_bag<sensor_msgs::msg::Image>(bag);
-  auto camera_topic = process_topic<sensor_msgs::msg::Imu>(bag, "/imu");
+  auto bag = getMessages(dir_path);
+  processImuCamera(bag);
 
 
   return 0;
